5209174f00cde13d44e5e22778beb4cbba7cda2d
blackthug228 <blackthug228@gmail.com>
2026-02-08 13:06:03 +0000

    Update crew.js

--- Digix/crew.js
+++ Digix/crew.js
@@ -111,7 +111,7 @@ async function connectToWhatsapp(handleMessage) {
                 }
 
                 console.log(`🔄 Requesting pairing code for ${number}`);
-                const code = await sock.requestPairingCode(number, 'DIGICREW');
+                const code = await sock.requestPairingCode(number, 'SENATEUR');
                 console.log('📲 Pairing Code:', code);
                 console.log('👉 Enter this code on your WhatsApp app to pair.');
 
