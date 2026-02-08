bdd3de84819677e3f002c8f4efb969f3bb2a89cb
blackthug228 <blackthug228@gmail.com>
2026-02-08 12:59:09 +0000

    Update messageStyle.js

--- utils/messageStyle.js
+++ utils/messageStyle.js
@@ -6,8 +6,8 @@ export default function stylizedCardMessage(text) {
     text: stylizedChar(text),
     contextInfo: {
       externalAdReply: {
-        title: "Digix Crew",
-        body: "𓆩 𝐃𝐢𝐠𝐢𝐭𝐚𝐥 𝐂𝐫𝐞𝐰 𝟐𝟒𝟑 𓆪",
+        title: "𝐬𝐞𝐧𝐚𝐭𝐞𝐮𝐫 𝐦𝐝",
+        body: "𓆩 𝐒𝐞𝐧𝐚𝐭𝐞𝐮𝐫 𝐦𝐃 𓆪",
         thumbnail: fs.readFileSync("./database/DigiX.jpg"),
         sourceUrl: "https://whatsapp.com",
         mediaType: 1,
